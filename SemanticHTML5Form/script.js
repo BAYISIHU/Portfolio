37a96a8c131d2d79f4bd424f08d332bedc04a1bf
hubayisi <hubayisi@gmail.com>
2025-10-26 13:13:43 +0900

    add custormer service

--- SemanticHTML5Form/script.js
+++
@@ -1,6 +0,0 @@
-// 获取 range input 和用于显示值的 span 元素
-const interestRange = document.getElementById('interest');
-const rangeValueSpan = document.getElementById('range-value');
-
-// TODO: 添加一个 input 事件监听器到 range input 上
-// 当滑块的值改变时，更新 span 标签中的文本内容，使其与滑块的当前值保持一致。
